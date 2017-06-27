08f321fa95c0f276aee4830a5e353b630c5ebc2c
Kevin Dai <Kevin-D-cw@users.noreply.github.com>
2017-06-27 00:46:18 -0400

    Update DesktopLauncher.java

--- desktop/src/com/mc/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mc/game/desktop/DesktopLauncher.java
@@ -9,10 +9,10 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
-        config.height = 832;
-        config.fullscreen = false;
-        config.resizable = false;
-        System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
+        	config.height = 832;
+        	config.fullscreen = false;
+        	config.resizable = false;
+        	System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
 		config.addIcon("windowLogo.png", Files.FileType.Internal);
 		new LwjglApplication(new BuildingRush(), config);
 	}
